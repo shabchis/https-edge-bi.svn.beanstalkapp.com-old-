43fcd99d87a04e428a6f1a579863f5445cda91f6
doron <doron@88400785-a41e-e944-ac52-453dfc92572b>
2010-12-22 10:18:04 +0000

    Building example data pipeline

---
+++ Core/trunk/Data.Pipeline/Readers/IDeliveryFileReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EdgeBI.Data.Pipeline
+{
+	public interface IDeliveryFileReader:IRowReader
+	{
+		public DeliveryFile File { get; set; }
+	}
+}
---
+++ Core/trunk/Data.Pipeline/Readers/XmlRowReader.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.IO;
+using System.Collections;
+
+namespace EdgeBI.Data.Pipeline
+{
+	public class XmlRowReader<RowT> : RowReader<RowT> where RowT: class
+	{
+		#region Members
+		/*=========================*/
+
+		public Func<XmlTextReader, RowT> OnNextRowRequired = null;
+		private string _url;
+		private XmlTextReader _xmlReader = null;
+
+		/*=========================*/
+		#endregion
+
+		#region Implementation
+		/*=========================*/
+
+		public XmlRowReader(string url)
+		{
+			if (String.IsNullOrEmpty(url))
+				throw new ArgumentNullException("url");
+			_url = url;
+		}
+
+		protected XmlTextReader XmlReader
+		{
+			get { return _xmlReader; }
+		}
+
+		protected override void Open()
+		{
+			_xmlReader = new XmlTextReader(_url);
+			_xmlReader.WhitespaceHandling = WhitespaceHandling.None;
+		}
+
+		protected override RowT NextRow()
+		{
+			if (OnNextRowRequired == null)
+				throw new InvalidOperationException("A delegate must be specified for OnNextRowRequired.");
+
+			return OnNextRowRequired(this.XmlReader);
+		}
+
+		public override void Dispose()
+		{
+			if (_xmlReader != null)
+				_xmlReader.Close();
+		}
+
+		/*=========================*/
+		#endregion
+	}
+}
