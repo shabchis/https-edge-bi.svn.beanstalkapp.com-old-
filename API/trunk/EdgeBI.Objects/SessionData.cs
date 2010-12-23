c85ba3064a1b67544dbbc28e7a1bfe821b513573
alonya <alonya@88400785-a41e-e944-ac52-453dfc92572b>
2010-12-23 05:53:11 +0000

    Fixing the LogIN after new http wcf

---
+++ API/trunk/EdgeBI.Objects/SessionData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace EdgeBI.Objects
+{
+	
+	public class SessionOperationData
+	{
+		
+		public string Email { get; set; }
+		
+		public string Password { get; set; }
+	}
+}
