e7a5473338e336b0a853ec8c897ca64b1963d7af
doron <doron@88400785-a41e-e944-ac52-453dfc92572b>
2011-01-26 12:18:09 +0000

    Editing some errors in the batch file.

--- Applications/Console/branches/frameless/Client/Certificates/Create Certificate.bat
+++ Applications/Console/branches/frameless/Client/Certificates/Create Certificate.bat
@@ -4,11 +4,12 @@ echo ----------------------------
 echo.
 echo NOTES:
 echo.
-echo (a) Password not needed for the first step, but required in the wizard.
+echo (1) Password not needed for the first step, but required in the wizard.
 echo.
-echo (b) Choose to export private key in the wizard, no strong protection, and save to the UI.Client\Certificates project folder.
+echo (2) Choose to export private key in the wizard, no strong protection, and save to the UI.Client\Certificates project folder. Double-click it and import, checking
+echo "allow it to be exported"
 echo.
-echo (c) After that, export public key only using SHA1 option by going to MMC->Certificates (Current User)->Personal->Certificates, right click export.
+echo (3) After that, export public key only using SHA1 option by going to MMC.exe Certificates snap-in, (Current User)\Personal\Certificates, right click export.
 echo Save it as "Deployment\Certificates\Certificate-public.cer"
 echo.
 pause
@@ -20,7 +21,7 @@ mkdir temp
 
 rem // You can change the email or the expire date here
 @echo on
-makecert -r -n "CN=Edge.BI,E=support@edge.bi" -sv temp\EdgeBI.pvk -b 01/01/2010 -e 01/01/2012  temp\EdgeBI.cer
+makecert -r -n "CN=Edge.BI,E=support@edge.bi" -sv temp\EdgeBI.pvk -b 01/01/2010 -e 01/01/2013  temp\EdgeBI.cer
 
 cert2spc temp\EdgeBI.cer temp\EdgeBI.spc
 
