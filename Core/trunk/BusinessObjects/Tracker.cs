a39df5a9a1f96ab8da4f8a3bba47a1b60a87c40c
doron <doron@88400785-a41e-e944-ac52-453dfc92572b>
2011-02-24 12:38:30 +0000

    Added Tracker class for regex patterns per accountID

---
+++ Core/trunk/BusinessObjects/Tracker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Easynet.Edge.Core.Data;
+using System.Text.RegularExpressions;
+
+namespace Easynet.Edge.BusinessObjects
+{
+	public class Tracker
+	{
+		public static string ExtractTracker(string url, string regex)
+		{
+			foreach (Group g in Regex.Match(url, regex).Groups)
+			{
+				if (!g.Success)
+					continue;
+
+				return g.Value;
+			}
+
+			// nothing found
+			return null;
+		}
+
+		public static string GetAccountTrackerPattern(int accountID)
+		{
+			/*
+			using (DataManager.Current.OpenConnection())
+			{
+				// SELECT GatewayBaseUrl FROM User_GUI_Account where
+			}
+			*/
+			throw new NotImplementedException();
+		}
+	}
+}
