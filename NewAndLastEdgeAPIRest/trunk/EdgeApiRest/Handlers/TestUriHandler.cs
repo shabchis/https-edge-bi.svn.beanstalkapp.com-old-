f9ec7a4702b84a1714c18f693d4be44d870658ff
alonya <alonya@88400785-a41e-e944-ac52-453dfc92572b>
2011-03-10 15:46:09 +0000

    First build of New and last rest api

---
+++ NewAndLastEdgeAPIRest/trunk/EdgeApiRest/Handlers/TestUriHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EdgeApiRest.Handlers.UriTemplate;
+
+namespace EdgeApiRest.Handlers
+{
+	public class TestUriHandler: UriTemplateHandler
+	{
+		public override bool ShouldValidateSession
+		{
+			get { return false; }
+		}
+
+		[UriMapping(Method = "GET", Template = "users/{userID}")]
+		public object Example(int userID)
+		{
+			return null;
+		}
+
+	}
+}
---
+++ NewAndLastEdgeAPIRest/trunk/EdgeApiRest/Handlers/UriTemplate/Attributes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace EdgeApiRest.Handlers.UriTemplate
+{
+	[AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+	public sealed class UriMappingAttribute : Attribute
+	{
+		public string Template { get; set; }
+		public string Method { get; set; }
+
+		public Regex Regex { get; set;  }
+	}	
+}
