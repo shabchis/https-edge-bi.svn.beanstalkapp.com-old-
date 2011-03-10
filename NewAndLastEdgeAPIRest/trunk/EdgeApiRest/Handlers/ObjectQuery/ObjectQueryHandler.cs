688a55b03e3df7b99aa869a0c4a31aa12e4d44de
alonya <alonya@88400785-a41e-e944-ac52-453dfc92572b>
2011-03-10 15:46:09 +0000

    First build of New and last rest api

---
+++ NewAndLastEdgeAPIRest/trunk/EdgeApiRest/Handlers/ObjectQuery/ObjectQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace EdgeApiRest
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class ObjectQueryHandler : BaseHandler
+	{
+		public override void ProcessRequest(HttpContext context)
+		{
+			ObjectQueryInfo info = new ObjectQueryInfo(context.Request.Path, context.Request.QueryString);
+			SetResponse(context, System.Net.HttpStatusCode.OK, info);
+		}
+	}
+
+
+}
