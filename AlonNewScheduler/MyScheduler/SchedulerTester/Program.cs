4832877a29c5500ddf18dff090ed4651c67680f7
alonyaari <alonyaari@88400785-a41e-e944-ac52-453dfc92572b>
2011-04-14 09:25:45 +0000

    Catch un handeled exceptiom=n

--- AlonNewScheduler/MyScheduler/SchedulerTester/Program.cs
+++ AlonNewScheduler/MyScheduler/SchedulerTester/Program.cs
@@ -13,9 +13,23 @@ namespace SchedulerTester
 		[STAThread]
 		static void Main()
 		{
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += new UnhandledExceptionEventHandler(currentDomain_UnhandledException);
+            Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new frmSchedulingControl());
 		}
+
+        static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message);
+        }
+
+        static void currentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception ex=(Exception)e.ExceptionObject;
+            MessageBox.Show(ex.Message);
+        }
 	}
 }
