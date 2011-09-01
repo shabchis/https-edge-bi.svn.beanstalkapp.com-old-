ed90caf057b90ab3a37f1bc5234df9b9ac1a34c1
doronassayas <doronassayas@88400785-a41e-e944-ac52-453dfc92572b>
2011-09-01 22:23:08 +0000

    Fixed deployment stuff

--- Applications/Console/branches/frameless/Deployment/Properties/Resources.Designer.cs
+++ Applications/Console/branches/frameless/Deployment/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Easynet.Edge.UI.Deployment.Prerequisites.Properties {
+namespace Easynet.Edge.UI.Deployment.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Easynet.Edge.UI.Deployment.Prerequisites.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Easynet.Edge.UI.Deployment.Prerequisites.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Easynet.Edge.UI.Deployment.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
